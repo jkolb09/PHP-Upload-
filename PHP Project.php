c0963ca7a40e35da1274d4d32b2dbb0cbd79b57d
jkolb09 <60716970+jkolb09@users.noreply.github.com>
2020-04-05 19:36:43 -0700

    Add files via upload

---
+++ PHP Project.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    
+</title>
+</head>
+
+<body>
+<form action="upload.php" method="POST" enctype= "multipart/form-data">
+
+<input type="file" name="file">
+    </form>
+<button type="submit" name="submit">UPLOAD
+</button>
+
+</body>
+
+</html>
